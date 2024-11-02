e7d599884a742b316f7440e4fb4f8c71cfb06ce1
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-03 01:49:04 +0900

    ホットリロードの設定

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()//これはHTTPリクエストを処理
-	r.GET("/ping", func(c *gin.Context) {//ルーターにクライアントのリクエスト先となるエンドポイントを追加
+	r.GET("/sample", func(c *gin.Context) {//ルーターにクライアントのリクエスト先となるエンドポイントを追加
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
