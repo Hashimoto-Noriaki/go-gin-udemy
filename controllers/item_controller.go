00dc12a06cc84d111fc44547504a050664b1f9e1
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-11 22:10:34 +0900

    コントローラーの実装

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -9,6 +9,7 @@ import (
 
 type IItemController interface {
     FindAll(ctx *gin.Context)
+    FindById(ctx *gin.Context)
 }
 
 type ItemController struct {
@@ -27,4 +28,23 @@ func (c *ItemController) FindAll(ctx *gin.Context) {
     }
 
     ctx.JSON(http.StatusOK, gin.H{"data": items})
+}
+
+func (c *ItemController) FindById(ctx *gin.Context){
+    itemId, err := strconv.ParseUnit(ctx.Params("id"),10,64)
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+        return 
+    }
+
+    item, err := c.service.FindById(unit(itemId))
+    if err != nil {
+        if err.Error() == "Item not found" {
+            ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+            return
+        }
+        ctx.JSON(http.StatusInternalServerError,gin.H{"error": "unexpected error"})
+        return
+    }
+    ctx.JSON(http.StatusOK, gin.H{"data": item})
 }
