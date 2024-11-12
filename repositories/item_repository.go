d2084ecc3a509555b7de3e634d987c25d9e42b56
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-12 21:53:13 +0900

    repositoryを実装

--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -10,6 +10,7 @@ type IItemRepository interface {
 	// repositoryが満たすべきメソッドを定義
 	FindAll() (*[]models.Item, error)               // 商品情報のリスト（Item構造体のスライス）へのポインタ
 	FindById(itemId int) (*models.Item, error) // itemIdの型をintに設定
+	Create(newItem models.Item)(*models.Item,error)
 }
 
 // ItemMemoryRepositoryはメモリ上でデータを保持するための構造体
@@ -35,4 +36,11 @@ func (r *ItemMemoryRepository) FindById(itemId int) (*models.Item, error) {
 		}
 	}
 	return nil, errors.New("Item not found")
+}
+
+// FindAllメソッドは、ItemMemoryRepository構造体のメソッド。
+func (r *ItemMemoryRepository) Create(newItem models.Item)(*models.Item,error){
+	newItem.ID = unit(len(r.items) + 1)
+	r.items = append(r.items,newItem)
+	return &newItem, nil
 }
