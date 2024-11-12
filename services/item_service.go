216ceff8e7b4e7bb8c371a6c2c1464bb3f7c8b57
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-12 22:07:20 +0900

    Serviceの実装

--- services/item_service.go
+++ services/item_service.go
@@ -9,6 +9,7 @@ import (
 type IItemServices interface {
     FindAll() (*[]models.Item, error)
     FindById(itemId int) (*models.Item, error) // itemIdの型をunitからintに修正
+    Create(CreateItemInput dto.CreateItemInput) (*models.Item, error)
 }
 
 // サービスの構造体定義
@@ -29,4 +30,14 @@ func (s *ItemServices) FindAll() (*[]models.Item, error) {
 // FindByIdメソッドの定義
 func (s *ItemServices) FindById(itemId int) (*models.Item, error) { // itemIdの型をunitからintに修正
     return s.repository.FindById(itemId)
+}
+
+func (s *ItemServices) Create(CreateItemInput dto.CreateItemInput) (*models.Item, error) { // itemIdの型をunitからintに修正
+    newItem := models.Item{
+        Name: createItemInput.Name,
+        Price: createItemInput.Price,
+        Description: createItemInput.Description,
+        SoldOut: false,
+    }
+    return s.repository.Create(newItem)
 }
