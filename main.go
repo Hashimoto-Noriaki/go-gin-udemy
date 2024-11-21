a3c7c1fbe71689c58547518a4759582d368a38dd
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-22 00:31:17 +0900

    ルーターのグルーピング

--- main.go
+++ main.go
@@ -26,10 +26,11 @@ func main() {
     itemController := controllers.NewItemController(itemService)
 
     r := gin.Default() // GinのデフォルトのHTTPルーターを作成
-    r.GET("/items", itemController.FindAll) // エンドポイントを追加
-    r.GET("/items/:id",itemController.FindById)
-    r.POST("/items",itemController.Create)
-    r.PUT("/items/:id",itemController.Update)
-    r.DELETE("/items/:id",itemController.Delete)
+    itemRouter := r.Group("/items")
+    r.GET("", itemController.FindAll) // エンドポイントを追加
+    r.GET("/:id",itemController.FindById)
+    r.POST("",itemController.Create)
+    r.PUT("/:id",itemController.Update)
+    r.DELETE("/:id",itemController.Delete)
     r.Run("localhost:8080")
 }
