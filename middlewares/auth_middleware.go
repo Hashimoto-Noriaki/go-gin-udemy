7136fb5f4975b302f6b9cee887ac79656f04302d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-25 22:57:50 +0900

    認証ミドルウェアの実装

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -1,9 +1,34 @@
 package middlewares
 
-import "go-gin-udemy/services"
+import (
+    "net/http"
+    "strings"
+
+    "github.com/gin-gonic/gin"
+    "go-gin-udemy/services"
+)
 
 func AuthMiddleware(authService services.IAuthServices) gin.HandlerFunc {
     return func(ctx *gin.Context) {
-        
+        header := ctx.GetHeader("Authorization")
+        if header == "" {
+            ctx.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+
+        if !strings.HasPrefix(header, "Bearer") {
+            ctx.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+
+        tokenString := strings.TrimPrefix(header, "Bearer")
+        user, err := authService.GetUserFromToken(tokenString)
+        if err != nil {
+            ctx.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+
+        ctx.Set("user", user)
+        ctx.Next()
     }
 }
